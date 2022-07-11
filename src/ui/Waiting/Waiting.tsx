3181af439b4c67f89c3cadc68f014e0e57677372
rersozlu <ersozlu.rafi@gmail.com>
2022-07-11 17:24:11 +0300

    added done animation

--- src/ui/Waiting/Waiting.tsx
+++ src/ui/Waiting/Waiting.tsx
@@ -6,6 +6,8 @@ import { Button } from "ui";
 import { useMediaQuery } from "react-responsive";
 import WaitingGif from "assets/gifs/loading.gif";
 import SkewLoader from "react-spinners/SkewLoader";
+import { MdOutlineDone } from "react-icons/md";
+import { motion } from "framer-motion";
 
 interface WaitingProps {
   functionName: string;
@@ -25,10 +27,20 @@ const Waiting = ({ functionName, iconName, value, icon }: WaitingProps) => {
         className={styles.exit}
         src={theme === "light" ? ExitBlack : ExitWhite}
       ></img>
-      <SkewLoader
+      {/* <SkewLoader
         className={styles.loader}
         color={theme === "light" ? "black" : "#fff"}
       />
+
+       */}
+      <motion.div
+        className={styles.done}
+        initial={{ opacity: 0, scale: 0.4 }}
+        animate={{ opacity: 1, scale: [0.8, 1.4, 0.9, 1.3, 1, 1.1, 1] }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+      >
+        <MdOutlineDone color="lightgreen" size={"50"} />
+      </motion.div>
 
       <div className={styles.text1}>Waiting For Confirmation</div>
 

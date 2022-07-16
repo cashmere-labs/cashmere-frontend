f10ead6b5c85f2a13591ed829f144fac20ccade3
Alim Şahin <alimsahin@Alim-MacBook-Air.local>
2022-07-16 21:55:14 +0300

    feat: executing modal 1/2

--- src/components/DAO/ExecutingModal/ExecutingModal.tsx
+++ src/components/DAO/ExecutingModal/ExecutingModal.tsx
@@ -2,6 +2,7 @@ import styles from "./ExecutingModal.module.scss";
 import ExitBlack from "assets/icons/exit-black.png";
 import ExitWhite from "assets/icons/exit-white.png";
 import { useTheme } from "hooks";
+import { Button, Input } from "ui";
 
 const ExecutingModal = () => {
   const { theme } = useTheme();
@@ -36,9 +37,83 @@ const ExecutingModal = () => {
           </div>
           <div>Pass Threshold</div>
           <div>
-            <div style={{width: `${barRate * 401}px`}}></div>
+            <div style={{ width: `${barRate * 401}px` }}></div>
           </div>
         </div>
+      </div>
+      <div>
+        <div className={styles.infoRow}>
+          <div>
+            <div>Creator</div>
+            <div>Jump Crypto</div>
+          </div>
+          <div>
+            <div>Total Amount</div>
+            <div>1,000 veCSM</div>
+          </div>
+        </div>
+        <div className={styles.infoRow}>
+          <div>
+            <div>Start time</div>
+            <div>Wed, Nov 10, 2021&nbsp;&nbsp;&nbsp;9:08:36 PM</div>
+          </div>
+          <div>
+            <div>Estimated end time</div>
+            <div>Wed, Nov 10, 2021&nbsp;&nbsp;&nbsp;9:08:36 PM</div>
+          </div>
+        </div>
+        <div className={styles.infoRow}>
+          <div>
+            <div>Description</div>
+            <div>Change the liquidation contract address on Overseer...</div>
+          </div>
+          <div>
+            <div>Extra Rewards </div>
+            <div>100,000 USDD to Yes supporters in Jump Crypto</div>
+          </div>
+        </div>
+      </div>
+      <div>BALANCE 24,689.905</div>
+      <div>
+        <Input
+          placeholder="Amount"
+          className={styles.input}
+          extendLeft
+          hideBorder
+        />
+        <Button
+          height="25px"
+          width="45px"
+          fontSize={"fs12"}
+          fontWeight="fw600"
+          onClick={() => {}}
+          color={theme === "light" ? "white" : "white"}
+        >
+          Max
+        </Button>
+        <div>VeCSM</div>
+      </div>
+      <div>
+        <Button
+          height="56px"
+          width="514px"
+          fontSize={"fs16"}
+          fontWeight="fw600"
+          onClick={() => {}}
+          color={theme === "light" ? "black" : "white"}
+        >
+          Yes
+        </Button>
+        <Button
+          height="56px"
+          width="514px"
+          fontSize={"fs16"}
+          fontWeight="fw600"
+          onClick={() => {}}
+          color={theme === "light" ? "transparentWhite" : "transparentBlack"}
+        >
+          No
+        </Button>
       </div>
     </div>
   );

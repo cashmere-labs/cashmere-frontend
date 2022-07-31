32353da83fc1d3fb7275d2c2f10f70e07d20b4cc
asgarovf <farhad@getwhelp.com>
2022-07-31 13:27:43 +0100

    fix: import error

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import { EthyleneProvider } from "ethylene/utils";
 import React from "react";
-import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "store";
 import "styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-const root = createRoot(document.getElementById("root") as HTMLElement);
 
 ReactDOM.render(
   <React.StrictMode>

38f905d2ec6000665a505f18d532902f7e7affd8
asgarovf <farhad@getwhelp.com>
2022-08-05 00:16:41 +0100

    feat: stats

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,6 +1,6 @@
 import styles from "./Test.module.scss";
 import { ResponsivePie } from "@nivo/pie";
-import { ExecutingModal, ProposalModal } from "components";
+import { ExecutingModal, ProposalModal, Statistics } from "components";
 import { useModal } from "hooks";
 
 const data = [
@@ -30,7 +30,7 @@ const Test = () => {
   return (
     <div style={{ width: "500px", height: "500px" }}>
       <div onClick={modal.open}>Hey</div>
-      <ExecutingModal modal={modal} />
+      <Statistics />
     </div>
   );
 };

983ef5ba799df1607ac7fb8fec973c758100fd98
asgarovf <farhad@getwhelp.com>
2022-08-07 15:03:16 +0100

    refactor: vecsm desktop table

--- src/components/VeCSM/DesktopTable/DesktopTable.tsx
+++ src/components/VeCSM/DesktopTable/DesktopTable.tsx
@@ -2,9 +2,9 @@ import styles from "./DesktopTable.module.scss";
 import { useTheme } from "hooks";
 import { ActiveValidators, InactiveValidators } from "../datas";
 import { Button } from "ui";
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { PATHS } from "constants/paths";
+import { Validator } from "types/app";
 
 interface Table {
   whichValidator: boolean;
@@ -23,76 +23,49 @@ const VeCSMDesktopTitle = () => {
   );
 };
 
-const VeCSMDesktopTable = ({ whichValidator, validatorCount }: Table) => {
+const Row = ({ data, i }: { data: Validator; i: number }) => {
   const { theme } = useTheme();
+
+  return (
+    <div className={styles.tableBody} key={i}>
+      <div className={styles.line}></div>
+      <div className={styles.datas}>
+        <div>{i + 1}</div>
+        <div>{data.name}</div>
+        <div>{data.votingPower} veCSM</div>
+        <div>%{data.commission}</div>
+        <div>
+          <Link to={`${PATHS.manage}/${data.id}`}>
+            <Button
+              height="40px"
+              width="156px"
+              color={
+                theme === "light" ? "transparentWhite" : "transparentBlack"
+              }
+              fontWeight="fw600"
+            >
+              Manage
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const VeCSMDesktopTable = ({ whichValidator, validatorCount }: Table) => {
   return (
     <>
       {whichValidator
-        ? InactiveValidators.map((data: any, i: number) => {
-            if (i < validatorCount) {
-              return (
-                <div className={styles.tableBody} key={i}>
-                  <div className={styles.line}></div>
-                  <div className={styles.datas}>
-                    <div>{i + 1}</div>
-                    <div>{data.name}</div>
-                    <div>{data.votingPower} veCSM</div>
-                    <div>%{data.commission}</div>
-                    <div>
-                      <Link to={`${PATHS.manage}/${data.id}`}>
-                        <Button
-                          height="40px"
-                          width="156px"
-                          color={
-                            theme === "light"
-                              ? "transparentWhite"
-                              : "transparentBlack"
-                          }
-                          fontWeight="fw600"
-                        >
-                          Manage
-                        </Button>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            } else {
-              return <Fragment key={i}></Fragment>;
-            }
+        ? InactiveValidators.map((data, i) => {
+            return (
+              i < validatorCount && <Row data={data} i={i} key={data.id} />
+            );
           })
         : ActiveValidators.map((data: any, i: number) => {
-            if (i < validatorCount) {
-              return (
-                <div className={styles.tableBody} key={i}>
-                  <div className={styles.line}></div>
-                  <div className={styles.datas}>
-                    <div>{i + 1}</div>
-                    <div>{data.name}</div>
-                    <div>{data.votingPower} veCSM</div>
-                    <div>%{data.commission}</div>
-                    <div>
-                      <Link to={`${PATHS.manage}/${data.id}`}>
-                        <Button
-                          height="40px"
-                          width="156px"
-                          color={
-                            theme === "light"
-                              ? "transparentWhite"
-                              : "transparentBlack"
-                          }
-                          fontWeight="fw600"
-                        >
-                          Manage
-                        </Button>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            } else {
-              return <Fragment key={i}></Fragment>;
-            }
+            return (
+              i < validatorCount && <Row data={data} i={i} key={data.id} />
+            );
           })}
     </>
   );
